200c22a5ebeece32c01e9b26f2d40188ced0fed5
Xavier Burrow <jxburrow@gmail.com>
2017-10-06 20:21:14 +1100

    material inspector

--- grid/index-single.php
+++ grid/index-single.php
@@ -28,15 +28,15 @@
           if ($type == 'type_static') :
             $image = get_field('image_single')['sizes']['large'];
             ?>
-            <img src='<?php echo $image;?>' />
+            <img class='img-fit' src='<?php echo $image;?>' />
         <?php
           elseif ($type == 'type_hover') :
             $images = get_field('image_double');
             $src1 = $images[0]['hover_image']['sizes']['large'];
             $src2 = $images[1]['hover_image']['sizes']['large'];
             ?>
-            <img class='reveal inverted display-none' src='<?php echo $src1;?>' />
-            <img class='reveal display-none' src='<?php echo $src2; ?>' />
+            <img class='reveal inverted display-none img-fit' src='<?php echo $src1;?>' />
+            <img class='reveal display-none img-fit' src='<?php echo $src2; ?>' />
         <?php
           elseif ($type == 'type_inspector') :
             get_template_part('inspector/inspector');
