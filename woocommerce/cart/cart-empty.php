b57fe1d02d003fa75940025f999bb8b98dbf7b0e
Xavier Burrow <jxburrow@gmail.com>
2017-10-11 19:44:06 +1100

    woo fix

--- woocommerce/cart/cart-empty.php
+++ woocommerce/cart/cart-empty.php
@@ -21,16 +21,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 wc_print_notices();
-
-/**
- * @hooked wc_empty_cart_message - 10
- */
-do_action( 'woocommerce_cart_is_empty' );
-
-if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
-	<p class="return-to-shop">
-		<a class="button wc-backward" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-			<?php _e( 'Return to shop', 'woocommerce' ) ?>
-		</a>
-	</p>
+?>
+<div class='cart-grid cart-empty text-normal'>
+<?php
+	do_action('woocommerce_cart_is_empty');
+	if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
+		<p class="return-to-shop">
+			<a class="button wc-backward" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
+				<?php _e( 'Return to shop', 'woocommerce' ) ?>
+			</a>
+		</p>
 <?php endif; ?>
+</div>
--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -1,31 +1,25 @@
 <?php
-/**
- * Cart Page
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/cart/cart.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see     https://docs.woocommerce.com/document/template-structure/
- * @author  WooThemes
- * @package WooCommerce/Templates
- * @version 3.1.0
- */
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 wc_print_notices();
-do_action( 'woocommerce_before_cart' ); ?>
+do_action( 'woocommerce_before_cart' );
+?>
 
-<form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
+<form class="cart-grid woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
 	<?php do_action( 'woocommerce_before_cart_table' ); ?>
 
+	<div class='grid cart-grid__products'>
+		<?php foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item):
+
+			?>
+
+
+		<?php endforeach ?>
+	</div>
+
 	<table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents" cellspacing="0">
+		<!--
 		<thead>
 			<tr>
 				<th class="product-remove">&nbsp;</th>
@@ -36,6 +30,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 				<th class="product-subtotal"><?php _e( 'Total', 'woocommerce' ); ?></th>
 			</tr>
 		</thead>
+	-->
 		<tbody>
 			<?php do_action( 'woocommerce_before_cart_contents' ); ?>
 
@@ -154,8 +149,6 @@ do_action( 'woocommerce_before_cart' ); ?>
 <div class="cart-collaterals">
 	<?php
 		/**
-		 * woocommerce_cart_collaterals hook.
-		 *
 		 * @hooked woocommerce_cross_sell_display
 		 * @hooked woocommerce_cart_totals - 10
 		 */
